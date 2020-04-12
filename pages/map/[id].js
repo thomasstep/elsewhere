23386d519f8d8b15fceccee3e9f5b421889d1090
thomasstep <tstep916@gmail.com>
2020-04-12 18:41:54 -0500

    Messing with map.

--- pages/map/[id].js
+++ pages/map/[id].js
@@ -1,19 +1,30 @@
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+
+import { getMarkers } from '../../utils/db';
+
+export async function getServerSideProps(context) {
+  const markers = getMarkers(context.params.id);
+  return {
+    props: {
+      markers,
+    },
+  }
+}
 
 
 export class ElsewhereMap extends React.Component {
+  
   render() {
     return (
       <Map google={this.props.google} zoom={14}>
+
+        {this.props.markers.map((marker) => 
+          <Marker
+            name={'Current location'}
+            position={marker}
+          />
+        )}
  
-        {/* <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow> */}
       </Map>
     );
   }
--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,28 @@
-
 const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+const sampleMarkers = {
+  '1': {
+      markers: [
+        {lat: 25.774, lng: -80.190},
+        {lat: 18.466, lng: -66.118},
+        {lat: 32.321, lng: -64.757},
+        {lat: 25.774, lng: -80.190}
+      ],
+  }
+}
+
+function getMarkers(mapId) {
+  return sampleMarkers[mapId].markers;
+}
+
+module.exports = {
+  getMarkers,
+};
