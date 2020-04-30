16b93f09794a545c19adabcc75f96267852623e0
thomasstep <tstep916@gmail.com>
2020-04-30 11:03:21 -0500

    getMarkers from client side works again.

--- pages/map/[id].js
+++ pages/map/[id].js
@@ -5,34 +5,29 @@ import { request } from 'graphql-request';
 
 import Layout from '../../components/layout';
 
-const fetcher = (query) => request('http://localhost:3000/api/graphql', query);
+const fetcher = (query) => request('/api/graphql', query);
 
 function ElsewhereMap(props) {
-  console.log(props)
   const { data, error } = useSWR(
     `{
-      getMarkers(map: ${props.url.query.id}) {
+      getMarkers(map: 1) {
         lat
         lng
       }
     }`,
     fetcher,
   );
-  console.log('Data:')
-  console.log(data)
-  console.log('Error:')
-  console.error(error)
 
   return (
     <Layout>
       <Box>
         <Map google={props.google} zoom={14}>
 
-          {data.getMarkers.map((marker) =>
+          {data ? data.getMarkers.map((marker) =>
             <Marker
               position={marker}
             />
-          )}
+          ) : null}
    
         </Map>
       </Box>
