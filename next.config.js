dcc32f182ca20698cf7bfcb894727334f12b3332
thomasstep <tstep916@gmail.com>
2020-05-04 13:57:52 -0500

    debugging zeit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 module.exports = {
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -27,6 +27,7 @@ const resolvers = {
   Query: {
     hello: (parent, args, context) => "Hello!",
     getMarkers: async (parent, args) => {
+      console.log(process.env)
       const { markers } = await maps.findOne({ map: args.map });
       return markers;
     },
