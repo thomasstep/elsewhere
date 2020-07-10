645acdf391e7ddb57796886c89f429de21434f61
thomasstep <tstep916@gmail.com>
2020-07-09 21:43:55 -0500

    Working on Google OAuth.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,19 +1,35 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import { log } from '../../../utils/log';
 
 const options = {
   site: process.env.SITE || 'http://localhost:3000',
 
   // Configure one or more authentication providers
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    Providers.Google({
+      clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
     }),
+    // Providers.Email({
+    //   server: process.env.EMAIL_SERVER,
+    //   from: process.env.EMAIL_FROM,
+    // }),
   ],
-
-  // A database is optional, but required to persist accounts in a database
-  database: process.env.DATABASE_URL,
+  database: {
+    type: 'mongodb',
+    url: process.env.MONGODB_FOR_AUTH,
+    entityPrefix: 'nextauth_',
+    useNewUrlParser: true,
+    // synchronize: true,
+  },
+  debug: true,
+  events: {
+    createUser: async (message) => {
+      log.info('USER CREATED', {message})
+      log.info(JSON.stringify(message))
+    },
+  },
 };
 
 export default (req, res) => NextAuth(req, res, options);
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/client';
 import Layout from '../components/layout';
 import { fetcher } from '../utils/fetcher';
 
@@ -11,30 +12,51 @@ const viewerQuery = `{
   }`;
 
 function Index() {
-  const [email, setEmail] = useState('');
-  const router = useRouter();
+  // const [email, setEmail] = useState('');
+  // const router = useRouter();
+  const [ session, loading ] = useSession()
 
-  useEffect(() => {
-    fetcher(viewerQuery)
-      .then(({ viewer: { email: userEmail } }) => {
-        setEmail(userEmail);
-      })
-      .catch(() => {
-        router.push('/signin');
-      });
-  });
+  // useEffect(() => {
+  //   fetcher(viewerQuery)
+  //     .then(({ viewer: { email: userEmail } }) => {
+  //       setEmail(userEmail);
+  //     })
+  //     .catch(() => {
+  //       router.push('/signin');
+  //     });
+  // });
 
-  if (email) {
-    return (
-      <Layout>
-        <main className="center">
-          Let&apos;s go Elsewhere.
-        </main>
-      </Layout>
-    );
-  }
+  // if (email) {
+  //   return (
+  //     <Layout>
+  //       <main className="center">
+  //         Let&apos;s go Elsewhere.
+  //       </main>
+  //     </Layout>
+  //   );
+  // }
 
-  return <p>Loading...</p>;
+  // return <p>Loading...</p>;
+
+  return (
+    <p>
+      {!session && (
+        <>
+          Not signed in
+          <br />
+          <a href="/api/auth/signin">Sign in</a>
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as
+          {session.user.email}
+          <br />
+          <a href="/api/auth/signout">Sign out</a>
+        </>
+      )}
+    </p>
+  );
 }
 
 export default Index;
