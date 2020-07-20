bdbe94354c11e452e0372291188d5bfc51e87095
thomasstep <tstep916@gmail.com>
2020-07-20 17:50:08 -0500

    Added special user creation logic into context creation.

--- apollo/context.js
+++ apollo/context.js
@@ -1,5 +1,6 @@
+const { v4 } = require('uuid');
 const { getSession, setOptions } = require('next-auth/client');
-const { users } = require('../utils');
+const { users, log } = require('../utils');
 
 setOptions({ site: process.env.SITE });
 
@@ -7,7 +8,32 @@ async function context(ctx) {
   const { req } = ctx;
   const { user } = await getSession({ req });
   if (user && user.email) {
-    const foundUser = await users.findOne({ email: user.email });
+    let foundUser = await users.findOne({ email: user.email });
+
+    // There are some instances in which a user will not be created by the NextAuth event
+    // A successful getSession call means they are a legitimate user
+    if (!foundUser) {
+      const uuid = v4();
+      try {
+        foundUser = await users.create(
+          {
+            uuid,
+            email: user.email,
+            ownedMaps: [],
+            writableMaps: [],
+            readableMaps: [],
+          },
+        );
+      } catch (err) {
+        log.error('Error creating new user.', {
+          nextauthUser: user,
+          uuid,
+          email: user.email,
+        });
+        log.error(err);
+      }
+    }
+
     ctx.user = foundUser;
     ctx.user.id = foundUser.uuid;
   }
