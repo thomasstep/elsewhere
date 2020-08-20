bef5cdc267973733e5f97d40a891d620f3ad8371
thomasstep <tstep916@gmail.com>
2020-08-19 20:02:41 -0500

    Adding connectMongo back in bc a reference do using this method in nextjs examples.

--- apollo/context.js
+++ apollo/context.js
@@ -1,13 +1,14 @@
 const cookie = require('cookie');
 const jwt = require('jsonwebtoken');
 const { AuthenticationError } = require('apollo-server-micro');
-const { users } = require('../utils/db');
+const { connectMongo, users } = require('../utils/db');
 const { log } = require('../utils');
 
 const { JWT_SECRET } = process.env;
 
 async function context(ctx) {
   const { req } = ctx;
+  await connectMongo();
 
   const { token } = cookie.parse(req.headers.cookie ?? '');
   if (token) {
--- utils/db.js
+++ utils/db.js
@@ -2,55 +2,63 @@ const mongoose = require('mongoose');
 const schemas = require('./dbSchemas');
 const { log } = require('./log');
 
-// async function connectMongo() {
-//   if (!mongoose.connection.readyStates) {
-//     mongoose.Promise = global.Promise;
-//     const options = {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       bufferCommands: false,
-//       dbName: 'elsewhere',
-//       w: 'majority',
-//       useFindAndModify: false,
-//     };
+const connection = {};
 
-//     try {
-//       log.info('Attempting to connect to DB.');
-//       await mongoose.connect(process.env.MONGODB_URI, options);
-//       log.info('Connected to DB.');
-//     } catch (e) {
-//       log.error('Caught error connecting to DB.');
-//       log.error(e);
-//     }
-//   } else {
-//     log.info('DB already connected.');
+async function connectMongo() {
+  if (connection.isConnected) {
+    log.info('DB already connected.');
+    return;
+  }
+
+  // if (!mongoose.connection.readyStates) {
+  //   mongoose.Promise = global.Promise;
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      bufferCommands: false,
+      dbName: 'elsewhere',
+      w: 'majority',
+      useFindAndModify: false,
+    };
+
+    try {
+      log.info('Attempting to connect to DB.');
+      const db = await mongoose.connect(process.env.MONGODB_URI, options);
+      log.info('Connected to DB.');
+      connection.isConnected = db.connections[0].readyState;
+    } catch (e) {
+      log.error('Caught error connecting to DB.');
+      log.error(e);
+    }
+  // } else {
+  //   log.info('DB already connected.');
+  // }
+}
+
+// if (!mongoose.connection.readyStates) {
+//   mongoose.Promise = global.Promise;
+//   const options = {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     bufferCommands: false,
+//     dbName: 'elsewhere',
+//     w: 'majority',
+//     useFindAndModify: false,
+//   };
+
+//   try {
+//     log.info('Attempting to connect to DB.');
+//     mongoose.connect(process.env.MONGODB_URI, options);
+//     log.info('Connected to DB.');
+//   } catch (e) {
+//     log.error('Caught error connecting to DB.');
+//     log.error(e);
 //   }
+// } else {
+//   log.info('DB already connected.');
 // }
 
 module.exports = {
-  // connectMongo,
+  connectMongo,
   ...schemas,
 };
-
-if (!mongoose.connection.readyStates) {
-  mongoose.Promise = global.Promise;
-  const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    bufferCommands: false,
-    dbName: 'elsewhere',
-    w: 'majority',
-    useFindAndModify: false,
-  };
-
-  try {
-    log.info('Attempting to connect to DB.');
-    mongoose.connect(process.env.MONGODB_URI, options);
-    log.info('Connected to DB.');
-  } catch (e) {
-    log.error('Caught error connecting to DB.');
-    log.error(e);
-  }
-} else {
-  log.info('DB already connected.');
-}
