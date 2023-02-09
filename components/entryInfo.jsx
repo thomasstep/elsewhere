107f1a364280665783b50dbb13e892599cea39fc
Thomas Step <tstep916@gmail.com>
2023-02-09 07:02:56 -0600

    No loading spinner for entry tabs since that doesn't make sense.

--- components/entryInfo.jsx
+++ components/entryInfo.jsx
@@ -10,7 +10,6 @@ import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { DateTime, Settings } from 'luxon';
 
-import LoadingPage from './loadingPage';
 import DateTimeField from './dateTimeField';
 
 Settings.defaultZone = 'utc';
@@ -25,226 +24,208 @@ function EntryInfo({
 }) {
   // Boolean tells whether the entry has been edited
   const [edited, setEdited] = useState(false);
+  // Using truthy on purpose
+  // eslint-disable-next-line eqeqeq
+  const activeEntryExists = activeEntry.id != false;
 
-  if (activeEntry.id) {
-    return (
-      <Box>
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="baseline"
-          wrap="nowrap"
-          spacing={6}
-        >
-          {/* Name field */}
-          <Grid item xs={12}>
-            <Grid
-              container
-              direction="row"
-              justify="center"
-              alignItems="center"
-              spacing={2}
-            >
-              <Grid item>
-                <TextField
-                  id="filled-basic"
-                  value={activeEntry.name ? activeEntry.name : ''}
-                  label="Name"
-                  variant="standard"
-                  onChange={(e) => {
-                    setActiveEntry({
-                      ...activeEntry,
-                      name: e.target.value,
-                    });
-                    setEdited(true);
-                  }}
-                />
-              </Grid>
+  return (
+    <Box>
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="baseline"
+        wrap="nowrap"
+        spacing={6}
+      >
+        {/* Name field */}
+        <Grid item xs={12}>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <Grid item>
+              <TextField
+                id="filled-basic"
+                value={activeEntry.name ? activeEntry.name : ''}
+                disabled={activeEntryExists}
+                label="Name"
+                variant="standard"
+                onChange={(e) => {
+                  setActiveEntry({
+                    ...activeEntry,
+                    name: e.target.value,
+                  });
+                  setEdited(true);
+                }}
+              />
             </Grid>
           </Grid>
+        </Grid>
 
-          {/* Created By field */}
-          {
-            activeEntry.createdBy ? (
-              <Grid item xs={12}>
-                <Typography variant="h5">Created By</Typography>
-                <Typography variant="body1">{activeEntry.createdBy}</Typography>
-              </Grid>
-            ) : null
-          }
+        {/* Created By field */}
+        {
+          activeEntry.createdBy ? (
+            <Grid item xs={12}>
+              <Typography variant="h5">Created By</Typography>
+              <Typography variant="body1">{activeEntry.createdBy}</Typography>
+            </Grid>
+          ) : null
+        }
 
-          {/* Notes field */}
-          <Grid item xs={12}>
-            <Grid
-              container
-              direction="row"
-              justify="center"
-              alignItems="center"
-              spacing={2}
-            >
-              <Grid item>
-                <TextField
-                  id="filled-basic"
-                  value={activeEntry.notes ? activeEntry.notes : ''}
-                  label="Notes"
-                  variant="standard"
-                  onChange={(e) => {
-                    setActiveEntry({
-                      ...activeEntry,
-                      notes: e.target.value,
-                    });
-                    setEdited(true);
-                  }}
-                  multiline
-                  rows={10}
-                />
-              </Grid>
+        {/* Notes field */}
+        <Grid item xs={12}>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <Grid item>
+              <TextField
+                id="filled-basic"
+                value={activeEntry.notes ? activeEntry.notes : ''}
+                disabled={activeEntryExists}
+                label="Notes"
+                variant="standard"
+                onChange={(e) => {
+                  setActiveEntry({
+                    ...activeEntry,
+                    notes: e.target.value,
+                  });
+                  setEdited(true);
+                }}
+                multiline
+                rows={10}
+              />
             </Grid>
           </Grid>
+        </Grid>
 
-          {/* Lat field */}
-          <Grid item xs={12}>
-            <TextField
-              id="filled-basic"
-              value={activeEntry.location ? activeEntry.location.latitude : ''}
-              label="Latitude"
-              variant="standard"
-              onChange={(e) => {
-                setActiveEntry({
-                  ...activeEntry,
-                  location: {
-                    ...activeEntry.location,
-                    latitude: e.target.value,
-                  },
-                });
-                setEdited(true);
-              }}
-            />
-          </Grid>
+        {/* Lat field */}
+        <Grid item xs={12}>
+          <TextField
+            id="filled-basic"
+            value={activeEntry.location ? activeEntry.location.latitude : ''}
+            disabled={activeEntryExists}
+            label="Latitude"
+            variant="standard"
+            onChange={(e) => {
+              setActiveEntry({
+                ...activeEntry,
+                location: {
+                  ...activeEntry.location,
+                  latitude: e.target.value,
+                },
+              });
+              setEdited(true);
+            }}
+          />
+        </Grid>
 
-          {/* Lng field */}
-          <Grid item xs={12}>
-            <TextField
-              id="filled-basic"
-              value={activeEntry.location ? activeEntry.location.longitude : ''}
-              label="Longitude"
-              variant="standard"
-              onChange={(e) => {
-                setActiveEntry({
-                  ...activeEntry,
-                  location: {
-                    ...activeEntry.location,
-                    longitude: e.target.value,
-                  },
-                });
-                setEdited(true);
-              }}
-            />
-          </Grid>
+        {/* Lng field */}
+        <Grid item xs={12}>
+          <TextField
+            id="filled-basic"
+            value={activeEntry.location ? activeEntry.location.longitude : ''}
+            disabled={activeEntryExists}
+            label="Longitude"
+            variant="standard"
+            onChange={(e) => {
+              setActiveEntry({
+                ...activeEntry,
+                location: {
+                  ...activeEntry.location,
+                  longitude: e.target.value,
+                },
+              });
+              setEdited(true);
+            }}
+          />
+        </Grid>
 
-          {/* Address field */}
-          <Grid item xs={12}>
-            <TextField
-              id="filled-basic"
-              value={activeEntry.location ? activeEntry.location.address : ''}
-              label="Address"
-              variant="standard"
-              onChange={(e) => {
-                setActiveEntry({
-                  ...activeEntry,
-                  location: {
-                    ...activeEntry.location,
-                    address: e.target.value,
-                  },
-                });
-                setEdited(true);
-              }}
-            />
-          </Grid>
+        {/* Address field */}
+        <Grid item xs={12}>
+          <TextField
+            id="filled-basic"
+            value={activeEntry.location ? activeEntry.location.address : ''}
+            disabled={activeEntryExists}
+            label="Address"
+            variant="standard"
+            onChange={(e) => {
+              setActiveEntry({
+                ...activeEntry,
+                location: {
+                  ...activeEntry.location,
+                  address: e.target.value,
+                },
+              });
+              setEdited(true);
+            }}
+          />
+        </Grid>
 
-          {/* Start time field */}
-          <Grid item xs={12}>
-            <DateTimeField
-              renderInput={(params) => <TextField {...params} />}
-              label="Start"
-              onChange={(val) => {
-                const isoString = val.toISO();
-                setActiveEntry({
-                  ...activeEntry,
-                  startTimestamp: isoString,
-                });
-                setEdited(true);
-              }}
-              value={activeEntry.startTimestamp ? activeEntry.startTimestamp : null}
-              maxDateTime={
-                activeEntry.endTimestamp
-                  ? DateTime.fromISO(activeEntry.endTimestamp)
-                  : null
-              }
-            />
-          </Grid>
+        {/* Start time field */}
+        <Grid item xs={12}>
+          <DateTimeField
+            renderInput={(params) => <TextField {...params} />}
+            disabled={activeEntryExists}
+            label="Start"
+            onChange={(val) => {
+              const isoString = val.toISO();
+              setActiveEntry({
+                ...activeEntry,
+                startTimestamp: isoString,
+              });
+              setEdited(true);
+            }}
+            value={activeEntry.startTimestamp ? activeEntry.startTimestamp : null}
+            maxDateTime={
+              activeEntry.endTimestamp
+                ? DateTime.fromISO(activeEntry.endTimestamp)
+                : null
+            }
+          />
+        </Grid>
 
-          {/* End time field */}
-          <Grid item xs={12}>
-            <DateTimeField
-              renderInput={(params) => <TextField {...params} />}
-              label="End"
-              onChange={(val) => {
-                const isoString = val.toISO();
-                setActiveEntry({
-                  ...activeEntry,
-                  endTimestamp: isoString,
-                });
-                setEdited(true);
-              }}
-              value={activeEntry.endTimestamp ? activeEntry.endTimestamp : null}
-              minDateTime={
-                activeEntry.startTimestamp
-                  ? DateTime.fromISO(activeEntry.startTimestamp)
-                  : null
-              }
-            />
-          </Grid>
+        {/* End time field */}
+        <Grid item xs={12}>
+          <DateTimeField
+            renderInput={(params) => <TextField {...params} />}
+            disabled={activeEntryExists}
+            label="End"
+            onChange={(val) => {
+              const isoString = val.toISO();
+              setActiveEntry({
+                ...activeEntry,
+                endTimestamp: isoString,
+              });
+              setEdited(true);
+            }}
+            value={activeEntry.endTimestamp ? activeEntry.endTimestamp : null}
+            minDateTime={
+              activeEntry.startTimestamp
+                ? DateTime.fromISO(activeEntry.startTimestamp)
+                : null
+            }
+          />
+        </Grid>
 
-          {/* Save and delete buttons */}
-          <Grid item xs={12}>
-            <Button
-              variant="contained"
-              // className={classes.saveButton}
-              startIcon={<SaveIcon />}
-              onClick={async () => {
-                if (edited) {
-                  // Make API call
-                  const updateRes = await updateEntry();
-                  if (!updateRes) {
-                    // TODO show error
-                    return;
-                  }
-
-                  // Update state accordingly
-                  const newEntries = Array.from(entries);
-                  const index = newEntries.findIndex((entry) => activeEntry.id === entry.id);
-                  if (index >= 0) {
-                    newEntries[index] = activeEntry;
-                    setEntries(newEntries);
-                  } else {
-                    // TODO show error because entry was not found
-                  }
-                }
-              }}
-            >
-              Save
-            </Button>
-
-            <Button
-              variant="contained"
-              // className={classes.deleteButton}
-              startIcon={<DeleteIcon />}
-              onClick={async () => {
+        {/* Save and delete buttons */}
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            disabled={activeEntryExists}
+            startIcon={<SaveIcon />}
+            onClick={async () => {
+              if (edited) {
                 // Make API call
-                const deleteRes = await deleteEntry();
-                if (!deleteRes) {
+                const updateRes = await updateEntry();
+                if (!updateRes) {
                   // TODO show error
                   return;
                 }
@@ -253,23 +234,46 @@ function EntryInfo({
                 const newEntries = Array.from(entries);
                 const index = newEntries.findIndex((entry) => activeEntry.id === entry.id);
                 if (index >= 0) {
-                  newEntries.splice(index, 1);
+                  newEntries[index] = activeEntry;
                   setEntries(newEntries);
                 } else {
                   // TODO show error because entry was not found
                 }
-              }}
-            >
-              Delete
-            </Button>
-          </Grid>
-        </Grid>
-      </Box>
-    );
-  }
+              }
+            }}
+          >
+            Save
+          </Button>
 
-  // TODO make this an error page
-  return <LoadingPage />;
+          <Button
+            variant="contained"
+            disabled={activeEntryExists}
+            startIcon={<DeleteIcon />}
+            onClick={async () => {
+              // Make API call
+              const deleteRes = await deleteEntry();
+              if (!deleteRes) {
+                // TODO show error
+                return;
+              }
+
+              // Update state accordingly
+              const newEntries = Array.from(entries);
+              const index = newEntries.findIndex((entry) => activeEntry.id === entry.id);
+              if (index >= 0) {
+                newEntries.splice(index, 1);
+                setEntries(newEntries);
+              } else {
+                // TODO show error because entry was not found
+              }
+            }}
+          >
+            Delete
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 EntryInfo.propTypes = {
--- components/newEntryForm.jsx
+++ components/newEntryForm.jsx
@@ -9,7 +9,6 @@ import SaveIcon from '@mui/icons-material/Save';
 import TextField from '@mui/material/TextField';
 import { DateTime, Settings } from 'luxon';
 
-import LoadingPage from './loadingPage';
 import DateTimeField from './dateTimeField';
 
 Settings.defaultZone = 'utc';
@@ -21,211 +20,207 @@ function NewEntryForm({
   setNewEntryData,
   createEntry,
 }) {
-  if (Object.keys(newEntryData).length > 0) {
-    return (
-      <Box>
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="baseline"
-          wrap="nowrap"
-          spacing={6}
-        >
-          {/* Name field */}
-          <Grid item xs={12}>
-            <Grid
-              container
-              direction="row"
-              justify="center"
-              alignItems="center"
-              spacing={2}
-            >
-              <Grid item>
-                <TextField
-                  id="filled-basic"
-                  value={newEntryData.name}
-                  label="Marker Name"
-                  variant="standard"
-                  onChange={(e) => {
-                    setNewEntryData({
-                      ...newEntryData,
-                      name: e.target.value,
-                    });
-                  }}
-                />
-              </Grid>
+  return (
+    <Box>
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="baseline"
+        wrap="nowrap"
+        spacing={6}
+      >
+        {/* Name field */}
+        <Grid item xs={12}>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <Grid item>
+              <TextField
+                id="filled-basic"
+                value={newEntryData.name}
+                label="Name"
+                variant="standard"
+                onChange={(e) => {
+                  setNewEntryData({
+                    ...newEntryData,
+                    name: e.target.value,
+                  });
+                }}
+              />
             </Grid>
-          </Grid>
-
-          {/* Notes field */}
-          <Grid item xs={12}>
-            <Grid
-              container
-              direction="row"
-              justify="center"
-              alignItems="center"
-              spacing={2}
-            >
-              <Grid item>
-                <TextField
-                  id="filled-basic"
-                  value={newEntryData.notes}
-                  label="Notes"
-                  variant="standard"
-                  onChange={(e) => {
-                    setNewEntryData({
-                      ...newEntryData,
-                      notes: e.target.value,
-                    });
-                  }}
-                  multiline
-                  rows={10}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-
-          {/* Lat field */}
-          <Grid item xs={12}>
-            <TextField
-              id="filled-basic"
-              value={newEntryData.location ? newEntryData.location.latitude : ''}
-              label="Latitude"
-              variant="standard"
-              onChange={(e) => {
-                setNewEntryData({
-                  ...newEntryData,
-                  location: {
-                    ...newEntryData.location,
-                    latitude: e.target.value,
-                  },
-                });
-              }}
-            />
-          </Grid>
-
-          {/* Lng field */}
-          <Grid item xs={12}>
-            <TextField
-              id="filled-basic"
-              value={newEntryData.location ? newEntryData.location.longitude : ''}
-              label="Longitude"
-              variant="standard"
-              onChange={(e) => {
-                setNewEntryData({
-                  ...newEntryData,
-                  location: {
-                    ...newEntryData.location,
-                    longitude: e.target.value,
-                  },
-                });
-              }}
-            />
-          </Grid>
-
-          {/* Address field */}
-          <Grid item xs={12}>
-            <TextField
-              id="filled-basic"
-              value={newEntryData.location ? newEntryData.location.address : ''}
-              label="Address"
-              variant="standard"
-              onChange={(e) => {
-                setNewEntryData({
-                  ...newEntryData,
-                  location: {
-                    ...newEntryData.location,
-                    address: e.target.value,
-                  },
-                });
-              }}
-            />
-          </Grid>
-
-          {/* Start time field */}
-          <Grid item xs={12}>
-            <DateTimeField
-              renderInput={(params) => <TextField {...params} />}
-              label="Start"
-              onChange={(val) => {
-                const isoString = val.toISO();
-                setNewEntryData({
-                  ...newEntryData,
-                  startTimestamp: isoString,
-                });
-              }}
-              value={newEntryData.startTimestamp ? newEntryData.startTimestamp : null}
-              maxDateTime={
-                newEntryData.endTimestamp
-                  ? DateTime.fromISO(newEntryData.endTimestamp)
-                  : null
-              }
-            />
-          </Grid>
-
-          {/* End time field */}
-          <Grid item xs={12}>
-            <DateTimeField
-              renderInput={(params) => <TextField {...params} />}
-              label="End"
-              onChange={(val) => {
-                const isoString = val.toISO();
-                setNewEntryData({
-                  ...newEntryData,
-                  endTimestamp: isoString,
-                });
-              }}
-              value={newEntryData.endTimestamp ? newEntryData.endTimestamp : null}
-              minDateTime={
-                newEntryData.startTimestamp
-                  ? DateTime.fromISO(newEntryData.startTimestamp)
-                  : null
-              }
-            />
-          </Grid>
-
-          {/* Save and delete buttons */}
-          <Grid item xs={12}>
-            <Button
-              variant="contained"
-              // className={classes.saveButton}
-              startIcon={<SaveIcon />}
-              onClick={async () => {
-                // Make API call
-                const createRes = await createEntry();
-                if (!createRes) {
-                  // TODO show error
-                  return;
-                }
-
-                // Update state accordingly
-                const newEntries = Array.from(entries);
-                newEntries.push(createRes);
-                setNewEntryData({});
-                setEntries(newEntries);
-              }}
-            >
-              Save
-            </Button>
-
-            <Button
-              variant="contained"
-              // className={classes.deleteButton}
-              startIcon={<DeleteIcon />}
-              onClick={() => {
-                setNewEntryData({});
-              }}
-            >
-              Cancel
-            </Button>
           </Grid>
         </Grid>
-      </Box>
-    );
-  }
 
-  return <LoadingPage />;
+        {/* Notes field */}
+        <Grid item xs={12}>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <Grid item>
+              <TextField
+                id="filled-basic"
+                value={newEntryData.notes}
+                label="Notes"
+                variant="standard"
+                onChange={(e) => {
+                  setNewEntryData({
+                    ...newEntryData,
+                    notes: e.target.value,
+                  });
+                }}
+                multiline
+                rows={10}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/* Lat field */}
+        <Grid item xs={12}>
+          <TextField
+            id="filled-basic"
+            value={newEntryData.location ? newEntryData.location.latitude : ''}
+            label="Latitude"
+            variant="standard"
+            onChange={(e) => {
+              setNewEntryData({
+                ...newEntryData,
+                location: {
+                  ...newEntryData.location,
+                  latitude: e.target.value,
+                },
+              });
+            }}
+          />
+        </Grid>
+
+        {/* Lng field */}
+        <Grid item xs={12}>
+          <TextField
+            id="filled-basic"
+            value={newEntryData.location ? newEntryData.location.longitude : ''}
+            label="Longitude"
+            variant="standard"
+            onChange={(e) => {
+              setNewEntryData({
+                ...newEntryData,
+                location: {
+                  ...newEntryData.location,
+                  longitude: e.target.value,
+                },
+              });
+            }}
+          />
+        </Grid>
+
+        {/* Address field */}
+        <Grid item xs={12}>
+          <TextField
+            id="filled-basic"
+            value={newEntryData.location ? newEntryData.location.address : ''}
+            label="Address"
+            variant="standard"
+            onChange={(e) => {
+              setNewEntryData({
+                ...newEntryData,
+                location: {
+                  ...newEntryData.location,
+                  address: e.target.value,
+                },
+              });
+            }}
+          />
+        </Grid>
+
+        {/* Start time field */}
+        <Grid item xs={12}>
+          <DateTimeField
+            renderInput={(params) => <TextField {...params} />}
+            label="Start"
+            onChange={(val) => {
+              const isoString = val.toISO();
+              setNewEntryData({
+                ...newEntryData,
+                startTimestamp: isoString,
+              });
+            }}
+            value={newEntryData.startTimestamp ? newEntryData.startTimestamp : null}
+            maxDateTime={
+              newEntryData.endTimestamp
+                ? DateTime.fromISO(newEntryData.endTimestamp)
+                : null
+            }
+          />
+        </Grid>
+
+        {/* End time field */}
+        <Grid item xs={12}>
+          <DateTimeField
+            renderInput={(params) => <TextField {...params} />}
+            label="End"
+            onChange={(val) => {
+              const isoString = val.toISO();
+              setNewEntryData({
+                ...newEntryData,
+                endTimestamp: isoString,
+              });
+            }}
+            value={newEntryData.endTimestamp ? newEntryData.endTimestamp : null}
+            minDateTime={
+              newEntryData.startTimestamp
+                ? DateTime.fromISO(newEntryData.startTimestamp)
+                : null
+            }
+          />
+        </Grid>
+
+        {/* Save and delete buttons */}
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            // className={classes.saveButton}
+            startIcon={<SaveIcon />}
+            onClick={async () => {
+              // Make API call
+              const createRes = await createEntry();
+              if (!createRes) {
+                // TODO show error
+                return;
+              }
+
+              // Update state accordingly
+              const newEntries = Array.from(entries);
+              newEntries.push(createRes);
+              setNewEntryData({});
+              setEntries(newEntries);
+            }}
+          >
+            Save
+          </Button>
+
+          <Button
+            variant="contained"
+            // className={classes.deleteButton}
+            startIcon={<DeleteIcon />}
+            onClick={() => {
+              setNewEntryData({});
+            }}
+          >
+            Clear
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 NewEntryForm.propTypes = {
